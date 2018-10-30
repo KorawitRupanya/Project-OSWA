48439550255778642e4a580e13ec3cf4865f6dae
KameriiJ <jenny.sirarat@gmail.com>
2018-10-30 18:04:12 +0700

    update template

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import AuctionNav from "./AuctionNav.js";
+import { Grid, Row, Col } from "react-bootstrap";
 import "../css/App.css";
 
 class App extends Component {
@@ -8,7 +9,16 @@ class App extends Component {
       <div className="App">
         <AuctionNav role="navigation" />
         <body>
-          <header>Hello World</header>
+          <Grid fluid>
+            <Row className="show-grid">
+              <Col id="first-col" xs={6} md={2}>
+                <strong>For side nav-bar</strong>
+              </Col>
+              <Col xs={12} md={10}>
+                <p id="paragraph">For auction product card</p>
+              </Col>
+            </Row>
+          </Grid>
         </body>
       </div>
     );
