1e971d5b3588e2e0965115a54475d3d188351953
KorawitRupanya <smart1micky268@gmail.com>
2018-10-31 02:25:38 +0700

    add some templete

---
+++ src/reactify-ui/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Posts from './posts/Posts'
+
+class App extends Component {
+  render() {
+    return (
+      <Posts />
+    );
+  }
+}
+
+export default App;
+ 
