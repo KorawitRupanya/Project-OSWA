ccf10ef4e5d2816e635759d01d3415c8f0bb3758
KameriiJ <jenny.sirarat@gmail.com>
2018-11-01 13:41:24 +0700

    added bidding detail card

--- src/components/AuctionPageTemplate.js
+++ src/components/AuctionPageTemplate.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ImageSlideshow from "./ImageSlideshow";
-import { Col } from "react-bootstrap";
+import { Col, Alert } from "react-bootstrap";
 import "../css/AuctionPageTemplate.css";
 
 class AuctionPageTemplate extends Component {
@@ -9,6 +9,9 @@ class AuctionPageTemplate extends Component {
       <div className="Auction-page">
         <Col id="image-slideshow" md={4} style={{ "margin-left": "20px" }}>
           <ImageSlideshow />
+        </Col>
+        <Col id="bidding-detail" md={6}>
+          <Alert bsStyle="warning" />
         </Col>
       </div>
     );
