30896096379507c65b753b1141bee16921306abc
KameriiJ <jenny.sirarat@gmail.com>
2018-11-01 13:29:15 +0700

    deleted unused components

--- src/components/AuctionPageTemplate.js
+++ src/components/AuctionPageTemplate.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import ImageSlideshow from "./ImageSlideshow";
-import { Col, Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import "../css/AuctionPageTemplate.css";
-import { left } from "glamor";
 
 class AuctionPageTemplate extends Component {
   render() {
