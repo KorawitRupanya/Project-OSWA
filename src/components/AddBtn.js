4da9353e16519958767e01e8b67f3ed598992f3b
harryboxer <zorrawit@gmail.com>
2018-11-03 18:23:31 +0700

    updated pop-up

--- src/components/AddBtn.js
+++ src/components/AddBtn.js
@@ -4,11 +4,25 @@ import {
   Button,
   Tooltip,
   Modal,
-  OverlayTrigger
+  OverlayTrigger,
+  Navbar,
+  FormControl,
+  FormGroup,
+  Row
 } from "react-bootstrap";
 import "../css/AddBtn.css";
+import axios from "axios";
 
 class AddBtn extends Component {
+  fileSelectHolder = event => {
+    this.setState({
+      selectedFile: event.target.file[0]
+    })
+  }
+  fileUploadHolder = () => {
+    axios.post();
+  }
+
   constructor(props, context) {
     super(props, context);
 
@@ -44,20 +58,57 @@ class AddBtn extends Component {
           bsSize="large"
           onClick={this.handleShow}
         >
-          Add
+          Create new...
         </Button>
 
         <Modal show={this.state.show} onHide={this.handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Create a new product card</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>Text in a modal</h4>
-            <p>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </p>
+            {/* <h4>Text in a modal</h4> */}
+            <Navbar>
+              <Row>
+                <Navbar.Form pullLeft>
+                  <FormControl
+                    id="input-name-area"
+                    type="name"
+                    placeholder="Product Name"
+                  />
+                </Navbar.Form>
+              </Row>
+              <Row>
+                <Navbar.Form pullLeft>
+                  <FormControl
+                    id="input-details-area"
+                    type="details"
+                    placeholder="details..."
+                  />
+                </Navbar.Form>
+              </Row>
+              <Row>
+                <Navbar.Form>
+                  <FormControl
+                    id="input-time-area"
+                    type="time"
+                    placeholder="bidding time"
+                  />
+                </Navbar.Form>
+              </Row>
+              <Row>
+                <Navbar.Form>
+                  <Row>
+                  <div>
+                    <input type="file" onChange={this.fileHolder} />{" "}
+                    <Button onClick={this.fileHolder} id="upload-button">Upload</Button>
+                  </div>{" "}
+                  </Row>
+                </Navbar.Form>
+              </Row>
+            </Navbar>
 
-            <h4>Popover in a modal</h4>
+            <h3>Input your auction product details here.</h3>
+
             <p>
               there is a{" "}
               <OverlayTrigger overlay={popover}>
@@ -65,7 +116,6 @@ class AddBtn extends Component {
               </OverlayTrigger>{" "}
               here
             </p>
-
             <h4>Tooltips in a modal</h4>
             <p>
               there is a{" "}
@@ -74,55 +124,9 @@ class AddBtn extends Component {
               </OverlayTrigger>{" "}
               here
             </p>
-
             <hr />
-
-            <h4>Overflowing text to show scroll behavior</h4>
-            <p>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-              ac consectetur ac, vestibulum at eros.
-            </p>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur
-              et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-              auctor.
-            </p>
-            <p>
-              Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-              cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-              dui. Donec ullamcorper nulla non metus auctor fringilla.
-            </p>
-            <p>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-              ac consectetur ac, vestibulum at eros.
-            </p>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur
-              et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-              auctor.
-            </p>
-            <p>
-              Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-              cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-              dui. Donec ullamcorper nulla non metus auctor fringilla.
-            </p>
-            <p>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-              ac consectetur ac, vestibulum at eros.
-            </p>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur
-              et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-              auctor.
-            </p>
-            <p>
-              Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-              cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-              dui. Donec ullamcorper nulla non metus auctor fringilla.
-            </p>
+            <h4>O</h4>
+            <p>a</p>
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.handleClose}>Close</Button>
