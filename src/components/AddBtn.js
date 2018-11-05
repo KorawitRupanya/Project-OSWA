2b6bc718aa101ff1ff9985e522919c8dbdc221fd
harryboxer <zorrawit@gmail.com>
2018-11-05 08:28:33 +0700

    edited Lorem

--- src/components/AddBtn.js
+++ src/components/AddBtn.js
@@ -43,29 +43,13 @@ class AddBtn extends Component {
   render() {
     const popover = (
       <Popover id="modal-popover" title="Terms and conditions">
-        Harry Potter and the Philosopher's Stone Chapter 1 of 18 CHAPTER ONE -
-        THE BOY WHO LIVED Next Chapter Mr. and Mrs. Dursley, of number four,
-        Privet Drive, were proud to say that they were perfectly normal, thank
-        you very much. They were the last people you'd expect to be involved in
-        anything strange or mysterious, because they just didn't hold with such
-        nonsense. Mr. Dursley was the director of a firm called Grunnings, which
-        made drills. He was a big, beefy man with hardly any neck, although he
-        did have a very large mustache. Mrs. Dursley was thin and blonde and had
-        nearly twice the usual amount of neck, which came in very useful as she
-        spent so much of her time craning over garden fences, spying on the
-        neighbors. The Dursleys had a small son called Dudley and in their
-        opinion there was no finer boy anywhere. The Dursleys had everything
-        they wanted, but they also had a secret, and their greatest fear was
-        that somebody would discover it. They didn't think they could bear it if
-        anyone found out about the Potters. Mrs. Potter was Mrs. Dursley's
-        sister, but they hadn't met for several years; in fact, Mrs. Dursley
-        pretended she didn't have a sister, because her sister and her
-        good-for-nothing husband were as unDursleyish as it was possible to be.
-        The Dursleys shuddered to think what the neighbors would say if the
-        Potters arrived in the street. The Dursleys knew that the Potters had a
-        small son, too, but they had never even seen him. This boy was another
-        good reason for keeping the Potters away; they didn't want Dudley mixing
-        with a child like that.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
       </Popover>
     );
     const tooltip = <Tooltip id="modal-tooltip">wow.</Tooltip>;
