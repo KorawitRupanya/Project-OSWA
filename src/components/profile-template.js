5aefdd5974517851d15642b37105b635caaf3f9b
harryboxer <zorrawit@gmail.com>
2018-11-08 15:41:18 +0700

    added sign in button methods

--- src/components/profile-template.js
+++ src/components/profile-template.js
@@ -1,33 +1,13 @@
 import React, { Component } from "react";
-import {
-  Button,
-  Jumbotron,
-  Col,
-  Thumbnail,
-  OverlayTrigger,
-  Popover
-} from "react-bootstrap";
+import { Jumbotron } from "react-bootstrap";
+import SignInBar from "./SignInButton";
 import "../css/profile-template.css";
-
-import pro from "../pictures/profile.png";
 
 class ProfileTemplate extends Component {
   render() {
     const plate = (
       <Jumbotron id="plate">
-        <img id="image" src={pro} alt={"Profile pictures"} />
-        <h3>Name: Hairy Harry</h3>
-        <p>
-          <li>Member ID :007</li>
-          <li>Call :191</li>
-          <li>3 Own Auctions</li>
-          <li>5 auctions were Bidded </li>
-        </p>
-        <p>
-          <Button bsStyle="primary">Edit Profile</Button>
-        </p>
-          <Button bsStyle="primary" md={10}>Sign Up</Button>
-          <Button md={10}>Sign In</Button>
+        <SignInBar />
       </Jumbotron>
     );
     return plate;
