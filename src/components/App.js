524fb8b049347bdc9a5f1e553da85727bba5bc4d
KameriiJ <jenny.sirarat@gmail.com>
2018-11-14 15:14:40 +0700

    show components

--- src/components/App.js
+++ src/components/App.js
@@ -14,11 +14,11 @@ class App extends Component {
         <Grid fluid>
           <Row className="show-grid">
             <Col id="first-col" md={2}>
-              <ProfileTemplate/>
+              <ProfileTemplate />
               <strong>For side nav-bar</strong>
             </Col>
             <Col id="second-col" md={8}>
-              {/* Component */}
+              <AuctionPageTemplate />
             </Col>
             <Col md={2}>
               <strong style={{ color: "orange" }}>For ad?</strong>
